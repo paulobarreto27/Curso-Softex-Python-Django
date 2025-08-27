905862ade0e088d48ce33a60dafd8ac1a1f3a3bb
Your Name <227082358+codebyalexandre@users.noreply.github.com>
2025-08-27 11:40:57 -0300

    Aula 8

---
+++ aula8/exercicio2.py
@@ -0,0 +1,5 @@
+numeros = (f"Digite seu número de telefone com 11 dígitos: ")
+while True:
+    print(f"Bem vindo.")
+    telefone = int(input (f"Digite seu número de telefone no formato XX XXXXX-XXXX"  )).isdigit()
+    break
---
+++ aula8/exercicio3.py
@@ -0,0 +1,8 @@
+a = 1
+e = 2
+i = 3
+o = 4
+u = 5
+
+frase = input (f"Digite uma frase: ").lower()
+print {frase}.replace("a", "1").replace("e", "2").replace("i", "3").replace("o", "4").replace("u", "5")
