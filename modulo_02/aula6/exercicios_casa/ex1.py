5deb6e0d037ecb78ab9f417b01734eefa1f0890d
Paulo Barreto <paulobarreto27@gmail.com>
2025-09-22 21:39:39 -0300

    Exercícios feitos

--- modulo_02/aula6/exercicios_casa/ex1.py
+++ modulo_02/aula6/exercicios_casa/ex1.py
@@ -2,3 +2,17 @@
 Crie uma classe chamada Pessoa. No "registro de nascimento" (__init__), 
 toda pessoa deve ter um nome e uma idade."""
 
+class Pessoa:
+    def __init__(self, nome: str, idade: int) -> None:
+        self.nome = nome
+        self.idade = idade
+
+    
+
+# cria um objeto do tipo classe Pessoa
+nova_pessoa = Pessoa("Paulo", 47)
+
+# nome e idade são atributos (variáves) da classe Pessoa
+print(nova_pessoa.nome)
+print(nova_pessoa.idade)
+
--- modulo_02/aula6/exercicios_casa/ex11.py
+++ modulo_02/aula6/exercicios_casa/ex11.py
@@ -6,3 +6,30 @@ Crie duas classes: Livro e Biblioteca.
 ○ adicionar_livro(livro): recebe um objeto Livro e o adiciona à lista acervo.
 ○ listar_livros(): percorre a lista acervo e imprime o título e o autor de cada livro.
 Crie uma biblioteca, crie alguns objetos Livro e adicione-os à biblioteca. Depois, liste os livros."""
+
+
+class Livro:
+    def __init__(self, titulo, autor):
+        self.titulo = titulo
+        self.autor = autor
+
+    def __str__(self):
+        return f"O livro {self.titulo} é do autor {self.autor}"
+
+class Biblioteca:
+    def __init__(self):
+        self.acervo = []
+
+    def adicionar_livros(self, livro:Livro):
+        self.acervo.append(livro)
+
+    def listar_livro(self):
+        for livro in self.acervo:
+            print(livro)
+
+livro1 = Livro("titulo1", "autor1")
+livro2 = Livro("titulo2", "autor2")
+biblioteca = Biblioteca()
+biblioteca.adicionar_livros(livro1)
+biblioteca.adicionar_livros(livro2)
+biblioteca.listar_livro()
