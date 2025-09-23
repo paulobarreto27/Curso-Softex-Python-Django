02add6bd49d419be373ca180bd9e3a6eecf4cb56
Paulo Barreto <paulobarreto27@gmail.com>
2025-09-22 23:11:51 -0300

    Exercício feito

--- modulo_02/aula6/exercicios_casa/ex5.py
+++ modulo_02/aula6/exercicios_casa/ex5.py
@@ -2,3 +2,16 @@
 Crie uma classe Conta Bancaria. Toda conta deve começar com um titular e um saldo inicial. 
 Crie um método depositar(valor) que some um valor ao saldo da conta. 
 Crie um objeto, deposite um valor e imprima o novo saldo."""
+
+class Conta_bancaria:
+    def __init__(self, nome: str, saldo: float) -> None:
+        self.nome = nome
+        self.saldo = saldo
+
+    
+correntista = Conta_bancaria("Paulo", 1000.00)
+
+print(correntista.nome)
+print(correntista.saldo)
+correntista.saldo += 1000.00
+print(correntista.saldo)
