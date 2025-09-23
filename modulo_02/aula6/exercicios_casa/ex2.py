7a8f455d70d7a75e58337023f77730bd3b8f51b1
Paulo Barreto <paulobarreto27@gmail.com>
2025-09-22 21:44:38 -0300

    Exercício feito

--- modulo_02/aula6/exercicios_casa/ex2.py
+++ modulo_02/aula6/exercicios_casa/ex2.py
@@ -3,3 +3,18 @@ Usando a classe Pessoa que você criou, crie dois objetos:
 1. Uma pessoa chamada "João", com 25 anos.
 2. Uma pessoa chamada "Maria", com 30 anos. Depois de criá-los, imprima o nome e 
 a idade de cada um para confirmar que deu certo."""
+
+class Pessoa:
+    def __init__(self, nome: str, idade: int) -> None:
+        self.nome = nome
+        self.idade = idade
+
+    
+
+# cria um objeto do tipo classe Pessoa
+nova_pessoa = Pessoa("João", 25)
+nova_pessoa = Pessoa("Maria", 30)
+
+# nome e idade são atributos (variáves) da classe Pessoa
+print(nova_pessoa.nome)
+print(nova_pessoa.idade)
