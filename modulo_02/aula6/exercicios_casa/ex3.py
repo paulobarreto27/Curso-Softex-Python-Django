ddbeb5d3aedb4a58ffec9f93fb175299fd0f0138
Paulo Barreto <paulobarreto27@gmail.com>
2025-09-22 22:28:59 -0300

    Exercício realizado

--- modulo_02/aula6/exercicios_casa/ex3.py
+++ modulo_02/aula6/exercicios_casa/ex3.py
@@ -1,3 +1,18 @@
 """Ensinando a se Apresentar
 Adicione um método (uma "ação") à sua classe Pessoa chamado apresentar. Esse método deve imprimir uma frase como: 
 "Olá, meu nome é [nome] e eu tenho [idade] anos." Chame esse método para os objetos "João" e "Maria"."""
+
+class Pessoa:
+    def __init__(self, nome: str, idade: int) -> None:
+        self.nome = nome
+        self.idade = idade
+
+    def apresentar(self) -> None:
+        print(f"Olá, meu nome é {self.nome} e eu tenho {self.idade} anos")
+
+
+nova_pessoa1 = Pessoa("João", 25)
+nova_pessoa2 = Pessoa("Maria", 30)
+
+nova_pessoa1.apresentar()
+nova_pessoa2.apresentar()
