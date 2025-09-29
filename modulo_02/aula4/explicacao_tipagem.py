57f72b0a4a5310630b4505fe6f6698b0f08b8780
paulobarreto27 <paulobarreto27@gmail.com>
2025-09-29 11:33:37 -0300

    Atualização

--- modulo_02/aula4/explicacao_tipagem.py
+++
@@ -1,5 +0,0 @@
-def sauda_com_idade(nome:str, idade:int) -> None:
-    """Ela recebe um nome string e uma idade inteiro e faz print dos dados"""
-    print(f"Olá {nome}, você tem {idade}!")
-
-sauda_com_idade("anderson", 42)
