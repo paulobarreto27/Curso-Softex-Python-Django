b345c7ce42f5d48b94b2da8cdbd0334e5da6cc21
kauazin21 <paulobarreto27@gmail.com>
2025-11-03 11:25:40 -0300

    Projeto django

---
+++ modulo_04/core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse("<h1>Olá, Mundo! Está é a minha primeira página Django!</h1>")
+
+def serviços(request):
+    return HttpResponse("<h1>Nossos Seviços</h1>")
