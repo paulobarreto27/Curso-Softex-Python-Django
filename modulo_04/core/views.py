9c6934114d668174a126ab3750967d2c444c47a9
kauazin21 <paulobarreto27@gmail.com>
2025-11-05 11:04:52 -0300

    Inclusões

--- modulo_04/core/views.py
+++ modulo_04/core/views.py
@@ -3,7 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Olá, Mundo! Está é a minha primeira página Django!</h1>")
+    context = {
+        'nome_usuario': 'Júnior',
+        'tecnologias': ['Python','Django','HTML','CSS'],
+    }
+    #return HttpResponse("<h1>Olá, Mundo! Está é a minha primeira página Django!</h1>")
+    return render(request, 'home.html', context)
+def login(request):
+    return HttpResponse("<input>Login</input>")
 
 def serviços(request):
     return HttpResponse("<h1>Nossos Seviços</h1>")
