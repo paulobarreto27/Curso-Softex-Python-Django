313e4a85e6aab30f8be388cbec46d42d82f09b25
paulobarreto27 <paulobarreto27@gmail.com>
2025-11-19 11:10:51 -0300

    Atualizações html

--- modulo_04/core/models.py
+++ modulo_04/core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Tarefa(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200) # equivale a TEXT no SQL
     concluida = models.BooleanField(default=False) # equivale a bool
     criada_em = models.DateTimeField(auto_now_add=True) 
