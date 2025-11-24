045a250ed14fd172b2b291a4bc2e07696aea1011
paulobarreto27 <paulobarreto27@gmail.com>
2025-11-24 11:00:41 -0300

    atualizações admin

--- modulo_04/core/admin.py
+++ modulo_04/core/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Tarefa
 
-# Register your models here.
-admin.site.register(Tarefa)
+class TarefaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'user',  'get_user_email', 'concluida', 'criada_em')
+    list_filter = ('concluida', 'user', 'criada_em')
+    search_fields = ('titulo', 'user__username')
+
+    fieldsets = (
+    ('Informações Principais', {
+    'fields': ('user', 'titulo')
+    }),
+    ('Status da Tarefa', {
+    'fields': ('concluida', 'criada_em')
+    }),
+    )
+
+    readonly_fields = ('criada_em',)
+    
+    @admin.display(description='Email do Usuário') # Define o título da coluna
+    def get_user_email(self, obj):
+        return obj.user.email
+
+admin.site.register(Tarefa, TarefaAdmin)
+ 
