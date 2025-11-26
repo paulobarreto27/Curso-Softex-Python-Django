28b122a04b33e0d762278cda06819d5991d28c35
paulobarreto27 <paulobarreto27@gmail.com>
2025-11-26 10:39:59 -0300

    Aula 8

--- modulo_04/core/models.py
+++ modulo_04/core/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from projects.models import Project
 
 class Tarefa(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200) # equivale a TEXT no SQL
     concluida = models.BooleanField(default=False) # equivale a bool
     criada_em = models.DateTimeField(auto_now_add=True) 
-
-    def __str__(self):
-        return self.titulo
+  project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tarefas') 
+     
+    titulo = models.CharField(max_length=200) 
+    concluida = models.BooleanField(default=False) 
+    criada_em = models.DateTimeField(auto_now_add=True) 
+ 
+def __str__(self): 
+    return self.titulo
