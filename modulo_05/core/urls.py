e0cf4a7ec75050bfe3c154152a1daf2e8e1cb9f0
Paulo Barreto <paulobarreto27@gmail.com>
2025-12-18 10:40:03 -0300

    Atualização Urls

--- modulo_05/core/urls.py
+++ modulo_05/core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ListaTarefasAPIView, DetalheTarefaAPIView
+from .views import ListaTarefasAPIView, DetalheTarefaAPIView, MinhaView, LogoutView
 
 app_name = 'core'
 urlpatterns = [
     path('tarefas/', ListaTarefasAPIView.as_view(), name='lista-tarefas'),
     path('tarefas/<int:pk>/', DetalheTarefaAPIView.as_view(), name='detalhe-tarefa'),
+    path('teste/', MinhaView.as_view(), name="teste-autenticado"),
+    path('logout/', LogoutView.as_view(), name='logout'), 
 ]
